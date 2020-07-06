1e1d19e03e49d14905ec2d0a3d97deb141db1fba
MartinReichel <66900529+MartinReichel@users.noreply.github.com>
2020-07-06 19:02:37 +0200

    var

--- script.js
+++ script.js
@@ -1,2 +1,23 @@
 document.write("script.js"); // Text s uvedeným textem script.js
-alert("The file is working"); // Vyskakující okno s upozorněním 
+// alert("The file is working"); // Vyskakující okno s upozorněním 
+document.write("<h2 style='color:blue;'>H2</h2>");
+document.write("<h1>Hello World!</h1>");
+document.write("<hr>"); // Podelná čára
+document.write("<p>This is a javascript tutorial</p>");
+
+var phrase="To be or not to be";
+
+document.write( phrase);
+document.write( phrase);
+document.write( phrase);
+phrase = "Orange";
+document.write( phrase);
+document.write( phrase);
+
+var phase = "Martin";
+var age = 29; 
+var gpa = 3.1;
+
+var isMale =  true; 
+var flaws = null; 
+var description = undefined; 
