2f4e8c280b0a184e68cb4e6a6ad1c1c2a198b795
MartinReichel <66900529+MartinReichel@users.noreply.github.com>
2020-07-12 10:21:43 +0200

    Pokus

--- Intro_to_jQuery/scripts2.js
+++ Intro_to_jQuery/scripts2.js
@@ -1,5 +1,13 @@
 $('p').css('color', 'purple');
 $('p').css('text-size-adjust', '50');
-$('.east').text('10');
 $('.south').text('Cool');
 $('.north').css('border-color', 'purple');
+
+let number = 10; 
+
+if (number > 100) {
+    console.log("whoah, that's a big number."); 
+  } else { 
+    console.log("just a regular number, please");
+  };
+  
---
+++ Manipulate the DOM with jQuery/scripts.js
@@ -0,0 +1,12 @@
+let names = ["Martin", "Michal", "Petr", "Jan"]; 
+
+$('ul').append('names');
+
+
+let additionalBlock = {
+    title: "Added with javascript",
+    text: "This block was added using JavaScript's jQuery library. How awesome!"
+  };
+
+  $('tittle').text('<p>Dokument</p>');
+  $("p").append( $("tittle") );
